db8ec1f5ba3ed939b6b8d749ac8a1a23b8d21ea2
luomi16 <254287020@qq.com>
2023-06-24 23:45:18 -0400

    travel list(1)

--- 3-Travel-List/src/App.js
+++ 3-Travel-List/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,15 +20,29 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   // not reload page
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -35,7 +50,12 @@ function Form() {
         ))}
       </select>
 
-      <input type="text" placeholder="Items..."></input>
+      <input
+        type="text"
+        placeholder="Items..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
       <button>ADD</button>
     </form>
   );
