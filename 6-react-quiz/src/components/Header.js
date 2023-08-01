9f2f78fdd0b91142a4e4186a90c7fd078d4c0efb
luomi16 <254287020@qq.com>
2023-08-01 14:49:12 -0400

    add icon rotate :) and create an app by vite

--- 6-react-quiz/src/components/Header.js
+++ 6-react-quiz/src/components/Header.js
@@ -1,7 +1,9 @@
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+    <header className="app-header">
+      <a href="https://react.dev" target="_blank">
+        <img src="logo512.png" className="logo react" alt="React logo" />
+      </a>
       <h1>The React Quiz</h1>
     </header>
   );
