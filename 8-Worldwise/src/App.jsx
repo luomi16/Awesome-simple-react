a28d43ccf4e02180123664fac4d3c8ba80455346
luomi16 <254287020@qq.com>
2023-08-02 23:58:01 -0400

    implement the cities list

--- 8-Worldwise/src/App.jsx
+++ 8-Worldwise/src/App.jsx
@@ -5,10 +5,39 @@ import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout"
 import PageNotFound from "./pages/PageNotFound"
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = 'http://localhost:8000'
 
 function App() {
-  return <div>
 
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        
+        console.log(data);
+        console.log(cities);
+      } catch {
+        alert("There was an error loading data...")
+      }finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+
+
+  return (
     <BrowserRouter>    
       <Routes>        
         <Route index element={<Homepage />} />
@@ -16,15 +45,15 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="countries" element={<CountryList />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound /> } />
       </Routes>
     </BrowserRouter>
-  </div>
+  );
 }
 
 export default App
---
+++ 8-Worldwise/src/components/CountryList.jsx
@@ -0,0 +1,10 @@
+import styles from './CountryList.module.css'
+function CountryList() {
+  return (
+    <ul className={styles.CountryList}>
+      LISTC
+    </ul>
+  )
+}
+
+export default CountryList
